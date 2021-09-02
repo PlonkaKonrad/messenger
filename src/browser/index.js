5b2effedbb51fd0dd4d2cb4d031392325fe03164
Konrad Płonka <konrad.plonka@ec-at.com>
2021-09-02 07:13:39 +0200

    initial commit

---
+++ src/browser/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { hydrate } from 'react-dom'
+import App from '../shared/App';
+import { BrowserRouter } from 'react-router-dom'
+// test
+
+
+hydrate(
+    <BrowserRouter>
+        <App coreCartId={window.__INITIAL_DATA__} />
+    </BrowserRouter>,
+    document.getElementById('app')
+)
